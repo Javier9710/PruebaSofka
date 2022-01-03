dc6c68a6fa23a725d6258dcba5fb1ccb1a317407
Javier Moncada <javierandresmd@ufps.edu.co>
2022-01-03 18:41:39 -0500

    base

---
+++ src/test/java/ufps/springBoot/juego/PreguntadosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ufps.springBoot.juego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreguntadosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
