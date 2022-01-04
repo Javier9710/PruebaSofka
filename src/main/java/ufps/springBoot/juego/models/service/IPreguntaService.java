bb12279a7bbc7a9540d458acf07e151f1cf05c8c
Javier Moncada <javierandresmd@ufps.edu.co>
2022-01-04 14:34:44 -0500

    creando metodo para registro de preguntas en service

--- src/main/java/ufps/springBoot/juego/models/service/IPreguntaService.java
+++ src/main/java/ufps/springBoot/juego/models/service/IPreguntaService.java
@@ -1,9 +1,25 @@
 package ufps.springBoot.juego.models.service;
 
 import org.springframework.stereotype.Service;
+import ufps.springBoot.juego.models.entities.Categoria;
 import ufps.springBoot.juego.models.entities.Pregunta;
+import ufps.springBoot.juego.models.entities.Respuesta;
+
+import java.util.List;
 
 public interface IPreguntaService {
 
     public Pregunta guardar(Pregunta pregunta);
+
+    public Pregunta getPregunta(Long id);
+
+    public List<Pregunta> listaPreguntas();
+
+    //----------------------------------------------
+
+    public List<Categoria> listaCategorias();
+    
+    //----------------------------------------------
+    
+    public Respuesta guardarRespuesta(Respuesta respuesta);
 }
--- src/main/java/ufps/springBoot/juego/models/service/PreguntaServiceImpl.java
+++ src/main/java/ufps/springBoot/juego/models/service/PreguntaServiceImpl.java
@@ -1,16 +1,58 @@
 package ufps.springBoot.juego.models.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ufps.springBoot.juego.models.dao.ICategoriaDao;
 import ufps.springBoot.juego.models.dao.IPreguntaDao;
+import ufps.springBoot.juego.models.dao.IRespuestaDao;
+import ufps.springBoot.juego.models.entities.Categoria;
 import ufps.springBoot.juego.models.entities.Pregunta;
+import ufps.springBoot.juego.models.entities.Respuesta;
 
+import java.util.List;
+
+@Service
 public class PreguntaServiceImpl implements IPreguntaService{
 
     @Autowired
     private IPreguntaDao preguntaDao;
 
+
+    @Autowired
+    private ICategoriaDao categoriaDao;
+    
+    @Autowired
+    private IRespuestaDao respuestaDao;
+
     @Override
     public Pregunta guardar(Pregunta pregunta) {
         return preguntaDao.save(pregunta);
     }
+
+    @Override
+    public Pregunta getPregunta(Long id) {
+        return preguntaDao.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Pregunta> listaPreguntas() {
+        return preguntaDao.findAll();
+    }
+
+    //-----------------------------------------------------
+
+    @Override
+    public List<Categoria> listaCategorias() {
+        return categoriaDao.findAll();
+    }
+
+    //--------------------------------------------------------
+    
+	@Override
+	public Respuesta guardarRespuesta(Respuesta respuesta) {
+		return respuestaDao.save(respuesta);
+	}
+
+
+
 }
