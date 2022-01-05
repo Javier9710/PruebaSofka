4504a40e492ec7be81cb843cedb79b0a4453d4e0
Javier Moncada <javierandresmd@ufps.edu.co>
2022-01-05 18:26:08 -0500

    Agregando preguntas de prueba en import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,9 +7,15 @@ INSERT INTO categorias (id_categoria,nombre) VALUES (5,'MUY DIFICIL');
 
 //Preguntas
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (1,'Cuanto es 1+1 ?', 1);
-INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (2,' cat 2 ?', 2);
-INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (3,'Cantidad de jugadores en un partido de Futbol', 1);
-INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (4,'Donde Nacio Simon Bolivar', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (2,' Cuanto es 5x5 ?', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (3,'Cantidad de jugadores en un partido de Futbol', 2);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (4,'Donde Nacio Simon Bolivar', 2);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (5,'Cual es la gravedad de la tierra ? ', 3);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (6,'Ciudad natal de Cristiano Ronaldo', 3);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (7,'Colores de la bandera de Francia', 4);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (8,'Cual es la capital Siecia ?', 4);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (9,'Cuanto tarda la tierra en dar una vuelta al sol ?', 5);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (10,'Pais mas pequeño del mundo', 5);
 
 
 //respuestas
@@ -18,10 +24,10 @@ INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (2,
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (3,'2', 1, 1);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (4,'99', 1, 0);
 
-INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (5,'res1', 2, 0);
-INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (6,'res2', 2, 1);
-INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (7,'res3', 2, 0);
-INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (8,'res4', 2, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (5,'25', 2, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (6,'10', 2, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (7,'0', 2, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (8,'15', 2, 0);
 
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (9,'12', 3, 0);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (10,'22', 3, 1);
@@ -32,6 +38,36 @@ INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (13
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (14,'Colombia', 4, 0);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (15,'Ecuador', 4, 0);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (16,'Peru', 4, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (17,' 9.8 m/s2', 5, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (18,' 10.8 m/s2', 5, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (19,' 10 m/s2', 5, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (20,' 0 m/s2', 5, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (21,'Madeira', 6, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (22,'Lisboa', 6, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (23,'Oporto', 6, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (24,'Funchal', 6, 1);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (25,'Blanco-Azul-Rojo', 7, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (26,'Amarillo-Azul-Rojo', 7, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (27,'Azul-Blanco-Rojo', 7, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (28,'Blanco-Rojo-Azul', 7, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (29,'Lund', 8, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (30,'Estocolmo', 8, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (31,'Orebro', 8, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (32,'Upsala', 8, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (33,'365 Dias y 6 Horas', 9, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (34,'356 Dias', 9, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (35,'365 Dias', 9, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (36,'365 Dias y 12 Horas', 9, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (33,'Maldivas', 10, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (34,'San Marino', 10, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (35,'Monaco', 10, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (36,'Vaticano', 10, 1);
 
 
 //premios
