b953e06bb3149b85b0e11e88f7d91b2585657744
Javier Moncada <javierandresmd@ufps.edu.co>
2022-01-06 16:25:40 -0500

    Se añadieron preguntas en import.sql de prueba(hay 23 preguntas, falta registrar 1 en categoria dificl y 1 en muy dificil para poder iniciar)

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,7 @@ INSERT INTO categorias (id_categoria,nombre) VALUES (5,'MUY DIFICIL');
 
 //Preguntas
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (1,'Cuanto es 1+1 ?', 1);
-INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (2,' Cuanto es 5x5 ?', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (2,'Cuanto es 5x5 ?', 1);
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (3,'Cantidad de jugadores en un partido de Futbol', 2);
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (4,'Donde Nacio Simon Bolivar', 2);
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (5,'Cual es la gravedad de la tierra ? ', 3);
@@ -16,6 +16,20 @@ INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (7,'Colores
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (8,'Cual es la capital Siecia ?', 4);
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (9,'Cuanto tarda la tierra en dar una vuelta al sol ?', 5);
 INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (10,'Pais mas pequeño del mundo', 5);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (11,' Cuanto es 10 + 10 ?', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (12,' Cuanto es 3 + 3 ?', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (13,' Cuanto es 999 * 0 ?', 1);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (14,'En qué lado del cuerpo está el hígado ?', 2);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (15,'Cómo se llama el líder de los Autobots en la saga de películas de “Transformers” ?', 2);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (16,'Qué famoso youtuber español tiene su propia serie de animación ?', 2);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (17,'Cuál es la velocidad de la luz ?', 3);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (18,'En qué mes el Sol está más cerca de la Tierra ?', 3);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (19,'Cómo se llama el caballo de Don Quijote de la Mancha ?', 3);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (20,'Con qué nombre firmaba Van Gogh sus obras ?', 4);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (21,'En que año inicio la Segunda Guerra Mundial', 4);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (22,'En qué año llegó el hombre a la Luna ?', 5);
+INSERT INTO preguntas (id_pregunta,descripcion, id_categoria) VALUES (23,'Qué país es el que, con 5 títulos, ha ganado más mundiales ?', 5);
+
 
 
 //respuestas
@@ -68,6 +82,71 @@ INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (37
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (38,'San Marino', 10, 0);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (39,'Monaco', 10, 0);
 INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (40,'Vaticano', 10, 1);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (41,'22', 11, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (42,'10', 11, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (43,'20', 11, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (44,'1010', 11, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (45,'7', 12, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (46,'0', 12, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (47,'33', 12, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (48,'6', 12, 1);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (49,'999', 13, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (50,'0', 13, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (51,'9990', 13, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (52,'1', 13, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (53,'Abajo', 14, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (54,'Arriba', 14, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (55,'Izquierdo', 14, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (56,'central-Derecho', 14, 1);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (57,'Optimus Prime', 15, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (58,'Bumblebee', 15, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (59,'Megatron', 15, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (60,'Jazz', 15, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (61,'The Gref', 16,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (62,'Dj Mario', 16, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (63,'Rubius', 16, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (64,'Giorgio', 16, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (65,'200 km/s', 17,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (66,'300.000 km/s', 17, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (67,'1000.000 km/s', 17, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (68,'100.000 km/s', 17, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (69,'Diciembre', 18,1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (70,'Enero', 18, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (71,'Julio', 18, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (72,'Agosto', 18, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (73,'Zeus', 19,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (74,'Troya', 19, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (75,'Caballito', 19, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (76,'Rocinante', 19, 1);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (77,'Drente', 20,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (78,'Vincent', 20, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (79,'Flevolanda', 20, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (80,'Vicente', 20, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (81,'1939', 21,1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (82,'1945', 21, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (83,'1912', 21, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (84,'1955', 21, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (85,'1985', 22,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (86,'1970', 22, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (87,'1969', 22, 1);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (88,'1975', 22, 0);
+
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (89,'España', 23,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (90,'Francia', 23, 0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (91,'Argentina', 23,0);
+INSERT INTO respuestas (id_respuesta,respuesta,id_pregunta, correcta) VALUES (92,'Brasil', 23, 1);
 
 
 //premios
